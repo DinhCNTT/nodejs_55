b450e8fca7dd2e9cf531aa15feead827e9f021fd
DinhCNTT <dinhcm123321@gmail.com>
2026-05-13 21:07:30 +0700

    Update database connection config and use URL

--- expressjs/src/common/prisma/connect.prisma.js
+++ expressjs/src/common/prisma/connect.prisma.js
@@ -1,12 +1,17 @@
 import "dotenv/config";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
 import { PrismaClient } from "./generated/prisma/index.js";
-console.log(process.env)
+import { DATABASE_URL } from "../constant/app.constant.js";
+
+const url = new URL(DATABASE_URL);
+
+console.log(url)
+
 const adapter = new PrismaMariaDb({
-    host: process.env.DATABASE_HOST,
-    user: process.env.DATABASE_USER,
-    password: process.env.DATABASE_PASSWORD,
-    database: process.env.DATABASE_NAME,
+    host: url.hostname,
+    user: url.username,
+    password: url.password,
+    database: url.pathname.substring(1),
     connectionLimit: 5,
 });
 const prisma = new PrismaClient({ adapter });
--- expressjs/src/services/article.service.js
+++ expressjs/src/services/article.service.js
@@ -1,6 +1,7 @@
 //service: xử lý logic nghiệp vụ, tương tác với database, sau đó trả về kết quả cho controller
 import sequelize from "../common/squelize/connect.sequelize.js";
 import Article from "../models/article.model.js";
+import { prisma } from "../common/prisma/connect.prisma.js";
 
 export const articleService = {
     async findAll() {
@@ -8,3 +9,4 @@ export const articleService = {
         return res;
     },
 };
+
